70f067d6df6a685b80fd9f1d30a65763838a7382
charleybbarkins <jaime.rodriguez@newtech.coppellisd.com>
2022-10-07 22:57:50 -0500

    arrayAnalysis lab

--- src/lab/ArrayAnalysis.java
+++ src/lab/ArrayAnalysis.java
@@ -1,5 +1,6 @@
 package lab;
 
+
 /*
  * See the link for lab explanation
  * https://docs.google.com/document/d/1QGhcNy-QOFT-J1rOHgeXKv81b5yz0LfEpL_Dl3PPff8/edit#
@@ -13,49 +14,130 @@ public class ArrayAnalysis {
 	//#1
 	public static int[][] createRandom(int row, int column, int min, int max){
 		
+		//create a new array using parameters (user input)
+		int[][] arr = new int[row][column];
 		
-		return null;
+		//while i is less than length..
+		for (int i = 0; i < arr.length; i++) {  //cycle through all the rows
+			for (int j = 0; j < arr[i].length; j++) {  //cycle through all the columns on a given row
+				
+				int scope = max - min + 1;
+				
+				//set array to random numbers within the parameters given 
+				arr[i][j] =  (int) (Math.random() * scope + min);  //setting the elements in the array
+				
+			}
+		}
+		
+		//return full array 
+		return arr;
 	}
+	
 	
 	
 	//#2
 	public static void printArray(int[][] arr)  {
 		
 		
+		for (int i = 0; i < arr.length; i++) { //cycle though all rows 
+			for (int j = 0; j < arr[i].length; j++) { //cycle through all columns
+				
+				//print array at every index
+				System.out.print(arr[i][j] + " ");
+				
+			}
+			//start new line to seperate all the arrays 
+			System.out.println();
+			
+		}
+		
+		
+	        
+    }
 	
-	}
+	
 	
 	
 	//#3
 	public static int sumTotal(int[][] arr) {
 		
+		//holder to keep sums 
+		int sum = 0;
 		
-		return 0;
+		//copy array rows onto int[] one 
+		for(int[] one: arr) {
+			
+			//copy columns from int[][] one onto int two
+			for (int two : one) {
+				
+				//add index two is at to sum counter
+				sum += two;
+				
+			}
+			
+		}
+		
+		//return sum of all numbers in array 
+		return sum;
 	}
 	
 	
 	//#4
 	public static double average(int[][] arr) {
 		
+		//calcuate the average using formula given (sum of array / # of #'s in array)
+		double av = (sumTotal(arr))/(arr.length * arr[0].length);
 		
 		
-		return 0;
+		//return the  average of all the numbers in array 
+		return av;
 	}
 	
 	
 	//#5
 	public static int findMin(int[][] arr) {
 		
+		//set min to largest possible number
+		int min = Integer.MAX_VALUE;
 		
-		return 0;
+		//while i is less than array length...
+		for (int i = 0; i < arr.length; i++) { //goes through all rows
+			for (int j = 0; j < arr[i].length; j++) { //goes thorugh all columns
+				
+				//if index is less than current number that is stored in min, replace min with taht number
+				//stores smallest number in array 
+				min = Math.min(min, arr[i][j]);
+				
+			}
+		
+			
+		}
+		
+		//return smallest number in array 
+		return min;
 	}
 	
 	
 	//#6
 	public static int findMax(int[][] arr) {
 		
+		//set max to smallest possible value 
+		int max = Integer.MIN_VALUE;
 		
-		return 0;
+
+		for (int i = 0; i < arr.length; i++) { //cylce through all rows
+			for (int j = 0; j < arr[i].length; j++) { //cycle though all columns
+				
+				//if number at index is bigger than number stored in max, replace max
+				//finds biggest number in array 
+				max = Math.max(max, arr[i][j]);
+				
+			}
+			
+		}
+		
+		//return biggest number in array 
+		return max;
 	}
 	
 	
@@ -63,32 +145,138 @@ public class ArrayAnalysis {
 	public static int[][] diffArrays(int[][] arr1, int[][] arr2){
 		
 		
+		//if arrays are not the smae length return null and end loop 
+		if (arr1.length != arr2.length) {
+			
+			return null;
+			
+			//if they are the same length,
+		}else {
+			
+			//create a new empty array with the same lengths as the compared arrays 
+			int[][] diff = new int[arr1.length][arr1[0].length];
+			
+			for (int i = 0; i < arr1.length; i++) {  //cycle through all the rows
+				for (int j = 0; j < arr1[i].length; j++) {  //cycle through all the columns on a given row
+					
+					//set diff index to store the difference between arr1 and arr2 at the same index
+					diff[i][j] =  Math.abs(arr1[i][j] - arr2[i][j]); 
+					
+					
+				}
+				
+			}
+			
+			//return diff (holding the differneces between arr1 and arr2 now)
+			return diff;
+		}
 		
-		return null;
+		
 	}
 	
 	
 	//#8
 	public static int[][] transposeRow(int[][] arr1, int dist){
 		
+		//keeps count of how many times array is transposed
+		int counter = 0;
 		
-		return null;
+		// while the loop has not repeated the same amount of times as dist,...
+		while (counter != dist) {
+		
+		/////////////////////////////
+			//create a new array that matches arr1's length 
+		int[][] diff = new int[arr1.length][arr1[0].length];
+		
+		//copy arr1 to diff
+		for (int i = 0; i < diff.length; i++) {  //cycle through all the rows
+			for (int j = 0; j < diff[i].length; j++) {  //cycle through all the columns on a given row
+				
+				
+				diff[i][j] =  arr1[i][j];  //set array diff to be the same as arr1
+				
+			}
+		}
+		//////////////////////////////////
+	
+	
+		for (int i = 0; i < arr1.length;i++ ) { //go through every row
+			
+			//store last row of array into int[] last
+			int[] last = diff[arr1.length-1];
+			
+			//keep count of how many times loop j has ran 
+			int count = 0;
+			for (int j = i + 1; j < arr1.length && count == 0; count++) { //goes though the row ahead of current row
+				
+			//if at the last row ...
+				if (j == arr1.length-1) {
+					
+					//set last row to equal previous row 
+					arr1[j]= arr1[j-1];
+					
+					//set the first row to equal last row 
+					arr1[0] = last;
+					
+				}else {
+					//create  holder for row
+					int[] holder;
+					
+					// set holder equal to row at i 
+					 holder = diff[i];
+					 
+					 //set j to holder (move the array down one)
+                    arr1[j] = holder;
+                    
+                  
+				}
+				
+				//store  last row in diff in int[] last
+				last = arr1[diff.length-1];
+				//set the last row in arr1 equal to the 2nd last row in diff (moves array down 1)
+				arr1[arr1.length-1] = diff[diff.length-2];
+			}
+			
+			
+		
+		}
+		
+		//add one to counter, continue loop until counter is equal to dist 
+		counter ++;
+		}
+		
+		//return transposed array
+		return arr1;
 	}
 	
 	
 	//#9
 	public static int sum3D(int[][][] arr) {
 		
+		//holds sum of numbers
+		int sum = 0;
 		
-		return 0;
+		for (int[][] one : arr){ //set int [][] one equal to arr
+			
+			//use sumtotal method to find the sum of array rows  and add it to sum 
+			sum +=sumTotal(one);
+			
+		}
+		
+		//return sum of 3D array 
+		return sum;
 	}
 	
 	
 	//#10
 	public static double average3D(int[][][] arr) {
 		
+		//use given fromula to find average of 3D array
+		// (total of all numbers from sum3d method / 3D array length)
+		double av = (sum3D(arr))/(arr.length * arr[0].length * arr[0][0].length );
 		
-		return 0;
+		//return average
+		return av;
 	}
 	
 	
--- src/lab/Runner.java
+++ src/lab/Runner.java
@@ -1,21 +1,188 @@
 package lab;
 
+import java.util.Scanner;
+
 public class Runner {
 
 	public static void main(String[] args) {
 		
+		//creates a new scanner 
+		Scanner inKey = new Scanner(System.in);
+		
+		//
 		ArrayAnalysis analysis = new ArrayAnalysis();
 		
-		int[][] arr1 = analysis.createRandom(6, 3, -2, 10);
+		//creates arr1 with using create random method
+		int[][] arr1 = ArrayAnalysis.createRandom(6, 3, -2, 10);
+		
+		//creates arr2 with the create random method 
+		int[][] arr3 = ArrayAnalysis.createRandom(6, 3, -2, 10);
+		
+		//prompts user to input parameters to make new 2D array
+		System.out.println("Input parameters for new 2D Array:\nHow many columns should your Array have?");
+		
+		//asks user for column input
+		int column = inKey.nextInt();
+		
+		//WHILE input is less than 4 or greater than 20 keep asking user to redo
+		while (column < 3 || column > 21 ) {
+			System.out.println("Bad input\nTry again with a number greater than 3 or less than 21");
+		 column = inKey.nextInt();
+		
+		}
+		
+		//get # of rows from input 
+		System.out.println("\nHow many rows should your Array have?");
+		
+		
+		int row = inKey.nextInt();
+		
+		//WHILE input is less than 4 or greater than 20 keep asking for another input 
+		while (row < 3 || row > 21 ) {
+			System.out.println("Bad input\nTry again with a number greater than 3 or less than 21");
+		 row = inKey.nextInt();
+		
+		}
+		
+		//ask user for minimum for random number creater
+		System.out.println("\nWhat is the minimum random numbers can be in your Array?");
+		
+		int min = inKey.nextInt();
+		
+		//WHILE minimum is a three digit number or greater keep asking user to inout a better input 
+		while (min > 99 || min < -99 ) {
+			System.out.println("Bad input\nTry again with a two digit number");
+		 min = inKey.nextInt();
+		
+		}
+		
+		//find max value in random numbers
+		System.out.println("\nWhat is the max value numbers can be in your Array?");
+		
+		//WHILE max is a three digit number keep asking user to different input 
+		int max = inKey.nextInt();	
+		while (max > 99 || max < -99 ) {
+			System.out.println("Bad input\nTry again with a two digit number");
+		 max = inKey.nextInt();
+		
+		}
+		
+		
+		//create arr2 using all the user inputs
+		int[][] arr2 = ArrayAnalysis.createRandom(row, column, min, max);
+		
+		
+		//print all of the above arrays created
+		System.out.println("\n-------------------");
+		System.out.println("Your Arrays:\nARRAY ONE");
+		ArrayAnalysis.printArray(arr1);
+		System.out.println("\nARRAY TWO");
+		ArrayAnalysis.printArray(arr2);
+		System.out.println("\nARRAY THREE");
+		ArrayAnalysis.printArray(arr3);
+		System.out.println("\n-------------------");
+		
+		
+		//find the sum of arr1 using sumTotal method 
+		System.out.print("\nSum of all numbers in Array 1: ");
+		System.out.println(ArrayAnalysis.sumTotal(arr1));
+		
+		//find the sum of arr2 using sumTotal method 
+		System.out.print("\nSum of all numbers in Array 2: ");
+		System.out.println(ArrayAnalysis.sumTotal(arr2));
+		
+		//find the sum of arr3 using sumTotal method 
+		System.out.print("\nSum of all numbers in Array 3: ");
+		System.out.println(ArrayAnalysis.sumTotal(arr3));
+		
+		////////////////////////////////////////////
+		
+		//find the average of all numbers in arr1 using the average method
+		System.out.print("\nAverage of numbers in Array One: ");
+		System.out.println(ArrayAnalysis.average(arr1));
+		
+		//find the average of all numbers in arr2 using the average method
+		System.out.print("\nAverage of numbers in Array Two: ");
+		System.out.println(ArrayAnalysis.average(arr2));
+		
+		//find the average of all numbers in arr3 using the average method
+		System.out.print("\nAverage of numbers in Array Three: ");
+		System.out.println(ArrayAnalysis.average(arr3));
+		///////////////////////////////////
+		
+		//find the samllest number present in array 1 using findMin method
+		System.out.print("\nThe smallest number in Array One: ");
+		System.out.println(ArrayAnalysis.findMin(arr1));
+		
+		//find the smallest number present in arr2 using findMin method
+		System.out.print("\nThe smallest number in Array Two: ");
+		System.out.println(ArrayAnalysis.findMin(arr2));
+		
+		//find the smallest number present in array 3 using findMin method
+		System.out.print("\nThe smallest number in Array Three: ");
+		System.out.println(ArrayAnalysis.findMin(arr3));
+		
+		/////////////////////////////////////////
+		
+		//find the biggest number present in arr1 using findMax method
+		System.out.print("\nThe biggest number in Array One: ");
+		System.out.println(ArrayAnalysis.findMax(arr1));
+		
+		//find the biggest number present in arr2 using findMax method
+		System.out.print("\nThe biggest number in Array Two: ");
+		System.out.println(ArrayAnalysis.findMax(arr2));
+		
+		//find the biggest number present in arr3 using findMax method
+		System.out.print("\nThe biggest number in Array Three: ");
+		System.out.println(ArrayAnalysis.findMax(arr3));
+		
+		////////////////////////////////
+		
+		//using diffArrays method, print out the difference between  arr1 and arr3 (if not equal returns null)
+		System.out.println("\nThe difference between Array 1 and Array 3: ");
+		ArrayAnalysis.printArray(ArrayAnalysis.diffArrays(arr1, arr3));
+		
+		System.out.println();
+		
+		//prompt user for how many rows to move arr2 down by 
+		System.out.println("How many rows would you like Array 2 to be moved down by?");
+		
+		//save user input
+		int diff = inKey.nextInt();
+		System.out.println();
+		System.out.println("Array 2 moved down by " + diff + " rows:");
+		//Use TransposeRow method to move array rows down by user input
+		ArrayAnalysis.printArray(ArrayAnalysis.transposeRow(arr2, diff));
 		
 		
 		
+		System.out.println("\n-------------------\n3D ARRAY");
+		
+		//create a new3D array 
+		int[][][] arr3D = new int[3][4][5];
+		for (int i = 0; i < arr3D.length; i++) {
+			
+			//set indexes to random numbers
+			arr3D[i] = ArrayAnalysis.createRandom(2, 2, -40, 40);
+		}
+		int x = 1;
+		
+		//print the 3D array
+		for (int[][] arr : arr3D) {
+			System.out.println("\nLayer #" + x);
+			ArrayAnalysis.printArray(arr);
+			x++;
+		}
+		System.out.println("\n-------------------");
 		
 		
+		//show the sum of all numbers in the 3D array using the sum3D method 
+		System.out.print("\nSum of all numbers in 3D Array: ");
+		System.out.println(ArrayAnalysis.sum3D(arr3D));
 		
-		
-		
-		
+		//show the average of all numbers in 3D array using average3D method
+		System.out.print("\nAverage of numbers in 3D Array: ");
+		System.out.println(ArrayAnalysis.average3D(arr3D));
 		
 		
 	}
